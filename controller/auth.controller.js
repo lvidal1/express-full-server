b76613ceff463f66bd14666fb8795f46c4e56d1a
lvidal1 <lvidal910@gmail.com>
2022-08-15 23:05:21 -0500

    [Auth] Add google sign-in

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,6 +1,7 @@
 const bcryptjs = require("bcryptjs");
 const { response } = require("express");
 const { generateJWT } = require("../helpers/generate-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 const User = require("../models/user");
 
 const login = async (req, res = response) => {
@@ -48,6 +49,54 @@ const login = async (req, res = response) => {
 
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { email, name, picture } = await googleVerify(id_token);
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            const data = {
+                email,
+                name,
+                role: 'admin',
+                google: true,
+                password: "-"
+            }
+
+            user = new User(data);
+            await user.save();
+        }
+
+        if (!user.status) {
+            return res.status(401).json({
+                msg: "User is now disabled"
+            })
+        }
+
+        const token = await generateJWT(user.id);
+
+        return res.status(200).json({
+            msg: 'Google signed in!',
+            token,
+            user
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            msg: "Google token was not able to be verified"
+        })
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
